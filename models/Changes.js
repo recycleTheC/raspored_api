f54b2832719a64ad5042b3abee4db22855507ad5
Mario Kopjar <mario.kopjar1@gmail.com>
2020-09-17 22:08:42 +0200

    Update Changes.js

--- models/Changes.js
+++ models/Changes.js
@@ -1,30 +1,33 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ChangesSchema = new Schema({
-  date: {
-    type: Date,
-    required: true,
-    default: new Date(),
-  },
-  classId: {
-    type: Number,
-    required: true,
-  },
-  changed: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "classes",
-  },
-  substitution: {
-    type: Schema.Types.ObjectId,
-    required: true,
-    ref: "classes",
-  },
-  location: {
-    type: String,
-    required: true,
-  },
+	date: {
+		type: Date,
+		required: true,
+		default: new Date(),
+	},
+	classId: {
+		type: Number,
+		required: true,
+	},
+	changed: {
+		type: Schema.Types.ObjectId,
+		ref: 'classes',
+	},
+	substitution: {
+		type: Schema.Types.ObjectId,
+		ref: 'classes',
+	},
+	location: {
+		type: String,
+	},
+	timeStart: {
+		type: String,
+	},
+	timeEnd: {
+		type: String,
+	},
 });
 
-module.exports = mongoose.model("changes", ChangesSchema);
+module.exports = mongoose.model('changes', ChangesSchema);
